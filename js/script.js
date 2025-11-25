f0dc1058b3f08b91b1d29753d4c6192532752eda
Julieta Maldonado <80077281+JulietaMaldonado@users.noreply.github.com>
2025-11-25 16:22:07 -0300

    creacion de mensajes de validacion en form

--- js/script.js
+++ js/script.js
@@ -2,13 +2,22 @@ let nombreapellido = document.getElementById("nombreApellido");
 let mail = document.getElementById("mail");
 let mensaje = document.getElementById("mensaje");
 let enviarbutton = document.getElementById("enviar");
+let errornombre = document.getElementById("errornombre");
+let errormail = document.getElementById("errormail");
+let errormensaje = document.getElementById("errormensaje");
+let mensajenvi = document.getElementById("mensajenvi");
 
 enviarbutton.addEventListener('click', function(event){
     event.preventDefault;
     console.log("Hola Mundo");
-    if(nombreapellido.value == ("") || mail.value == ("") || mensaje.value == ("")){
-        alert("No puedes dejar estos campos vacios.");
+    if(nombreapellido.value == ("")){
+        errornombre.textContent = "No puedes dejar esta casilla vacia.";
+    } if(mail.value == ("")){
+        errormail.textContent = "No puedes dejar esta casilla vacia.";
+    } if(mensaje.value == ("")){
+       errormensaje.textContent = "No puedes dejar esta casilla vacia.";
     }else{
-        alert("Formulario enviado!")
+        mensajenvi.textContent = "¡Mensaje enviado!"
+        //alert("Formulario enviado!")
     }
 });
